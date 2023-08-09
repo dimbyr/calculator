7ca65a221b97e2d3fcc0ae8b92ccb5e2c9ea872d
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-08-09 23:39:30 +0200

    Added functions that do the operations: additon, mult, div, subs; Added eventlistener for the numerical buttons to write them in the screen at click.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-screen = document.querySelector("#screen");
-buttons = document.querySelectorAll(".button");
+const screen = document.querySelector("#screen");
+const buttons = document.querySelectorAll(".button");
+const clearButton = document.querySelector(".clearButton");
+
+let screenContent = ""
+
+clearButton.addEventListener("click", () => {
+    screen.textContent = "0";
+    screenContent = "";
+})
 
 buttons.forEach((element) => { 
     element.addEventListener("mousedown", 
@@ -11,5 +19,55 @@ buttons.forEach((element) => {
         (e)=> { 
                 e.target.style["background-color"] = "#FFFFFF"
             }
-    )
+    );
+    
+    element.addEventListener("click",
+        (e) => {
+            screenContent += `${e.target.value}`;
+            if (screenContent.length < 13){
+            screen.textContent = screenContent;
+            };
+        })
 });
+
+
+
+
+
+
+
+function add(x,y){
+    return x+y;
+}
+
+function substract(x,y){
+    return x-y;
+}
+
+function multiply (x,y){
+    return x*y;
+};
+
+function divide(num, den){
+    if (den == 0){
+        if (num == 0){
+            return "error";
+        } else {
+        return Infinity;
+     }
+    } else {
+        return Math.round(100000000*num/den)/100000000;
+    };
+};
+
+function intDivide(num, den){
+    if (den == 0){
+        if (num == 0){
+            return "error";
+        } else {
+        return Infinity;
+     }
+    } else {
+        return Math.floor(num/den);
+    };
+};
