13635b555478b558858f68ae4c138fc83f708b22
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-08-14 23:14:40 +0200

    Everything required by TOP is satisfied. I need to create another branch if I want to edit this later.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const clearButton = document.querySelector(".clearButton");
 const operatorButtons = document.querySelectorAll(".operator");
 const deleteButton = document.querySelector(".delete");
 const commaButton = document.querySelector(".comma");
+const equalButton = document.querySelector(".equalButton");
 const OPERATORS = ["+", "-", "x", "/", "%", "="];
 
 let screenContent = "";
@@ -14,11 +15,13 @@ clearButton.addEventListener("click", () => {
     screenContent = "";
     operation = [];
     commaButton.disabled = false;
+    equalButton.disabled = false;
 });
 
 deleteButton.addEventListener(
     "click",
     () => { 
+        equalButton.disabled = false;
         screenContent = screenContent.slice(0,-1);
         if (screenContent.length!=0){
         screen.textContent = screenContent;
@@ -51,6 +54,7 @@ buttons.forEach((element) => {
     
     element.addEventListener("click",
         (e) => {
+            equalButton.disabled = false;
             screenContent = screenContent + e.target.value;
             if (true){
             screen.textContent = screenContent;
@@ -65,27 +69,62 @@ operatorButtons.forEach(
     elt => elt.addEventListener(
         "click",
         e => {
+            equalButton.disabled = false;
+
             let currentOp = e.target.value;
-            commaButton.disabled = false;
-            if (screenContent){
-            operation.push(screenContent);
+            commaButton.disabled = false; //unlock the comma button
+            if (screenContent){ // add the current collected number to the operations
+            operation.push(Number(screenContent));
             screenContent = ""
             };
             
             if (operation.length != 0){
                 let op = operation[operation.length -1];
                 if (!OPERATORS.includes(op)){
+                    // If the last typed button was an operator and it is followed by 
+                    // an operator, then just keep that last one
                     operation.push(currentOp);
                 } else {
                     operation[operation.length -1] = currentOp;
                 };
-            } else {
-                // operation[operation.length -1] = currentOp;
-            }
+            };
+
+           compute();
+
             console.log(operation);
         }
     )
 );
+
+equalButton.addEventListener(
+    "click",
+    () => {
+        if (screenContent && operation.length != 0){
+            operation.push(Number(screenContent));
+            compute();
+        } else if (operation.length !=0) {
+            if (operation[0]){
+            screen.textContent = operation[0];}
+            else {
+                screen.textContent ='0';
+            };
+        } else {
+            screen.textContent = '0';
+        };
+    }
+)
+
+function compute (){
+    if (operation.length >= 3) {
+        let firstNumber = Number(operation[0]);
+        let secondNumber =  Number(operation[2]);
+        let currentOperator = operation[1];
+        let nextOperator = operation[operation.length-1];
+        let liveResult = operate(firstNumber, secondNumber, currentOperator);
+        screen.textContent = liveResult;
+        operation = [liveResult, nextOperator];
+    } ;
+}
 
 function operate(firstNumber = 0, secondNumber = 0, op = "+"){
     switch(op){
@@ -93,7 +132,7 @@ function operate(firstNumber = 0, secondNumber = 0, op = "+"){
             return add(firstNumber, secondNumber);
         case "-":
             return substract(firstNumber,secondNumber);
-        case "*":
+        case "x":
             return multiply(firstNumber, secondNumber);
         case "/": 
             return divide(firstNumber, secondNumber);
@@ -104,7 +143,7 @@ function operate(firstNumber = 0, secondNumber = 0, op = "+"){
 }
 
 function add(x = 0, y = 0){
-    return x+y;
+    return myRound(x+y);
 };
 
 function substract(x = 0, y = 0){
@@ -112,7 +151,7 @@ function substract(x = 0, y = 0){
 };
 
 function multiply (x = 0,y = 1){
-    return x*y;
+    return myRound(x*y);
 };
 
 function divide(num = 0, den = 1){
@@ -123,18 +162,20 @@ function divide(num = 0, den = 1){
         return Infinity;
      }
     } else {
-        return Math.round(100000000*num/den)/100000000;
+        return myRound(num/den);
     };
 };
 
 function intDivide(num = 0, den = 1){
     if (den == 0){
-        if (num == 0){
-            return "error";
-        } else {
-        return Infinity;
-     }
-    } else {
+         return "error";
+     } else if (num.toString().includes('.') || den.toString().includes('.')){
+        return "error";
+     } else {
         return num%den;
     };
 };
+
+function myRound(x){
+    return Math.round(100000000*x)/100000000;
+}
